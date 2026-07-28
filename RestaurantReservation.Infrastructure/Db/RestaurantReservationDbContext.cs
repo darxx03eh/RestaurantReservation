02f0a5a6d76b24638891546baf1d31e17dc9ff89
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-28 10:46:57 +0300

    Initialize clean architecture, configure DbContext, and set up database connection

---
+++ RestaurantReservation.Infrastructure/Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace RestaurantReservation.Infrastructure.Db;
+
+public class RestaurantReservationDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public RestaurantReservationDbContext() => _configuration = LoadConfigurations();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("RestaurantReservationDbLocalConnection"));
+
+    private IConfiguration LoadConfigurations()
+        => new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+}
---
+++ RestaurantReservation/Program.cs
@@ -0,0 +1,11 @@
+﻿using RestaurantReservation.Infrastructure.Db;
+
+namespace RestaurantReservation;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        
+    }
+}
