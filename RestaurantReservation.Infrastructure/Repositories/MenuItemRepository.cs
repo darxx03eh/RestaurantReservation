9a17fb561481a5301a2628acc2e10f875011971a
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-29 10:29:47 +0300

    feature: retrieved a list of ordered items for sepcific reservation

--- RestaurantReservation.Infrastructure/Repositories/MenuItemRepository.cs
+++ RestaurantReservation.Infrastructure/Repositories/MenuItemRepository.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantReservation.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Domain.Entities;
 using RestaurantReservation.Infrastructure.Db;
 using RestaurantReservation.Infrastructure.IRepositories;
 using RestaurantReservation.Infrastructure.Repositories.Generic;
@@ -9,4 +10,18 @@ public class MenuItemRepository(RestaurantReservationDbContext context)
     : GenericRepository<MenuItem>(context), IMenuItemRepository
 {
     private readonly RestaurantReservationDbContext _context = context;
+    public async Task<IEnumerable<MenuItem>> ListOrderedMenuItemsAsync(int reservationId)
+    {
+        var items = await _context.Orders
+            .AsNoTracking()
+            .Where(o => o.ReservationId.Equals(reservationId))
+            .SelectMany(o => o.OrderItems)
+            .Select(oi => oi.Item)
+            .Distinct()
+            .ToListAsync();
+
+        return !items.Any()
+            ? Enumerable.Empty<MenuItem>()
+            : items;
+    }
 }
