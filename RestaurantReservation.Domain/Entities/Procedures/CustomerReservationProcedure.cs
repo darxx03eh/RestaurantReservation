a4c74c77930babb22e4bee16843ac398c357cd97
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-29 13:12:44 +0300

    migrations: write a migration that create a procedure to find customers that has a party size more than specific value

---
+++ RestaurantReservation.Domain/Entities/Procedures/CustomerReservationProcedure.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantReservation.Domain.Entities.Procedures;
+
+public class CustomerReservationProcedure
+{
+    public int CustomerId { get; set; }
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
