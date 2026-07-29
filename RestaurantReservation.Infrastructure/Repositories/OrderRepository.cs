906dba929ce5135ed7485cd4cf8a8d95f83c0b7d
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-29 10:19:14 +0300

    feature: list orders and menu items for specific reservation

--- RestaurantReservation.Infrastructure/Repositories/OrderRepository.cs
+++ RestaurantReservation.Infrastructure/Repositories/OrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantReservation.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Domain.Entities;
 using RestaurantReservation.Infrastructure.Db;
 using RestaurantReservation.Infrastructure.IRepositories;
 using RestaurantReservation.Infrastructure.Repositories.Generic;
@@ -9,4 +10,16 @@ public class OrderRepository(RestaurantReservationDbContext context)
     : GenericRepository<Order>(context), IOrderRepository
 {
     private readonly RestaurantReservationDbContext _context = context;
+    public async Task<IEnumerable<Order>> ListOrdersAndMenuItemsAsync(int reservationId)
+    {
+        var orders = await _context.Orders.AsNoTracking()
+            .Include(o => o.OrderItems)
+            .ThenInclude(oi => oi.Item)
+            .Where(o => o.ReservationId.Equals(reservationId))
+            .ToListAsync();
+
+        return !orders.Any()
+            ? Enumerable.Empty<Order>()
+            : orders;
+    }
 }
