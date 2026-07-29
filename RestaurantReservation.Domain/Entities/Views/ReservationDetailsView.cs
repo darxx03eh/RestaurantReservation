e04610bad0ca33df6a7ac5037fcaef2f6b01e415
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-29 11:26:06 +0300

    migrations: write a migration that create view contains reservation details with customer and restaurant

---
+++ RestaurantReservation.Domain/Entities/Views/ReservationDetailsView.cs
@@ -0,0 +1,17 @@
+﻿namespace RestaurantReservation.Domain.Entities.Views;
+
+public class ReservationDetailsView
+{
+    public int ReservationId { get; set; }
+    public DateTime ReservationDate { get; set; }
+    public int PartySize { get; set; }
+    public int TableId { get; set; }
+    
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerPhoneNumber { get; set; }
+    
+    public int RestaurantId { get; set; }
+    public string RestaurantName { get; set; }
+    public string RestaurantAddress { get; set; }
+}
