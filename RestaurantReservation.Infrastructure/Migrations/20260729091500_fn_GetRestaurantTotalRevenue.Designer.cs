25340fc92f483b3663aafd316cebd4b6a10b9684
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-29 12:31:01 +0300

    migrations: write a migration that create a function to find total revenue for a specific restaurant

--- RestaurantReservation.Infrastructure/Migrations/20260729091500_fn_GetRestaurantTotalRevenue.Designer.cs
+++ RestaurantReservation.Infrastructure/Migrations/20260729091500_fn_GetRestaurantTotalRevenue.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantReservation.Infrastructure.Db;
 
@@ -11,9 +12,11 @@ using RestaurantReservation.Infrastructure.Db;
 namespace RestaurantReservation.Infrastructure.Migrations
 {
     [DbContext(typeof(RestaurantReservationDbContext))]
-    partial class RestaurantReservationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260729091500_fn_GetRestaurantTotalRevenue")]
+    partial class fn_GetRestaurantTotalRevenue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RestaurantReservation.Infrastructure/Migrations/20260729091500_fn_GetRestaurantTotalRevenue.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fn_GetRestaurantTotalRevenue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var functionSql =
+                """
+                CREATE FUNCTION [dbo].[fn_GetRestaurantTotalRevenue]
+                    (@RestaurantId INT) RETURNS DECIMAL(18, 2)
+                AS
+                    BEGIN
+                        DECLARE @TotalRevenue DECIMAL(18, 2)
+                        SELECT @TotalRevenue = SUM([oi].Quantity * [i].Price)
+                        FROM [dbo].[Orders] AS [o]
+                        JOIN [dbo].[Reservations] AS [r] ON [o].[ReservationId] = [r].[ReservationId]
+                        JOIN [dbo].[OrderItems] AS [oi] ON [o].[OrderId] = [oi].[OrderId]
+                        JOIN [dbo].[MenuItems] AS [i] ON [oi].[ItemId] = [i].[ItemId]
+                        WHERE [r].[RestaurantId] = @RestaurantId
+                        RETURN ISNULL(@TotalRevenue, 0)
+                    END
+                """;
+            migrationBuilder.Sql(functionSql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+            => migrationBuilder.Sql("DROP FUNCTION IF EXISTS [dbo].[fn_GetRestaurantTotalRevenue]");
+    }
+}
