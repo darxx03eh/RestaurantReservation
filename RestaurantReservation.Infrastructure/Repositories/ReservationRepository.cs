948008ae0ed60d96ad1adaae7d55a8252335ef10
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-07-29 09:46:17 +0300

    feature: get reservations for specific customer  by his id

--- RestaurantReservation.Infrastructure/Repositories/ReservationRepository.cs
+++ RestaurantReservation.Infrastructure/Repositories/ReservationRepository.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantReservation.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Domain.Entities;
 using RestaurantReservation.Infrastructure.Db;
 using RestaurantReservation.Infrastructure.IRepositories;
 using RestaurantReservation.Infrastructure.Repositories.Generic;
@@ -9,4 +10,15 @@ public class ReservationRepository(RestaurantReservationDbContext context)
     : GenericRepository<Reservation>(context), IReservationRepository
 {
     private readonly RestaurantReservationDbContext _context = context;
+    public async Task<IEnumerable<Reservation>> GetReservationsByCustomerAsync(int customerId)
+    {
+        var reservations = await _context.Reservations.AsNoTracking()
+            .Include(r => r.Restaurant)
+            .Where(r => r.CustomerId.Equals(customerId))
+            .ToListAsync();
+
+        return !reservations.Any()
+            ? Enumerable.Empty<Reservation>()
+            : reservations;
+    }
 }
